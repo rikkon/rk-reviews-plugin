e07b670c8d91ffcbc3987a8d87f734ab638db112
Rikard O <rikard@rikkon.se>
2018-10-17 08:02:14 +0200

    Initial

---
+++ rk-reviews-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Rk Reviews Plugin
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Rikard
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
